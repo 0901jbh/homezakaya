f6d9030dca7977e1d5465b7051219f18926f4560
SujeongJo <suz.dev33@gmail.com>
2023-01-25 17:50:25 +0900

    feat: 회원관리 Controller, Mapper

--- backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/controller/UserController.java
+++ backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/controller/UserController.java
@@ -1,4 +1,59 @@
 package com.ssafy.Homezakaya.controller;
 
+import com.ssafy.Homezakaya.model.dto.UserDto;
+import com.ssafy.Homezakaya.model.service.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    @Autowired
+    private UserServiceImpl userService;
+
+    // 확인용
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+
+    // 회원 정보 등록
+    @PostMapping
+    public ResponseEntity<String> createUser(UserDto userDto) {
+        userService.createUser(userDto);
+        System.out.println("userDto = " + userDto);
+
+        return new ResponseEntity<>(SUCCESS, HttpStatus.CREATED);
+    }
+
+    // 회원 정보 조회
+    @GetMapping("/id/{userId}")
+    public ResponseEntity<?> getUser(@PathVariable String userId){
+        if(userService.getUser(userId) == null){
+            return new ResponseEntity<String>(FAIL,HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<UserDto>(userService.getUser(userId), HttpStatus.OK);
+    }
+
+    // 회원 정보 수정
+    @PutMapping("/{userId}")
+    public ResponseEntity<?> modifyUser(UserDto userDto){
+        userService.modifyUser(userDto);
+        return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+    }
+
+    // 회원 정보 삭제
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> removeUser(@PathVariable String userId){
+        if(userService.removeUser(userId)){
+            return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(FAIL, HttpStatus.NO_CONTENT);
+    }
+
+    // 로그인
+
+    // 로그아웃
+
 }
--- backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/model/dto/UserDto.java
+++ backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/model/dto/UserDto.java
@@ -1,22 +1,23 @@
 package com.ssafy.Homezakaya.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Date;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class UserDto {
     private String userId;
     private String password;
     private String nickname;
     private String email;
-    private Date birthDate;
+    private String birthDate;
     private String gender;
     private double mannerPoint;
     private double alcoholPoint;

14026c262766696e3e13fe4ec9edcdba103040db
SujeongJo <suz.dev33@gmail.com>
2023-01-25 10:49:57 +0900

    Remove Init, Backend Init

---
+++ backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/HomezakayaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.Homezakaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomezakayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomezakayaApplication.class, args);
+	}
+
+}

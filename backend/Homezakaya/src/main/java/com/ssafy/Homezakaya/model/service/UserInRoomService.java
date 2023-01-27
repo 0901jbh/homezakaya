ce3ddc6192c631c95bec88b884e04e4918805a77
9jinseok <mer0120@naver.com>
2023-01-27 15:07:15 +0900

    gjs::resolve:: resolve conflicts

--- backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/model/service/UserInRoomService.java
+++ backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/model/service/UserInRoomService.java
@@ -3,13 +3,7 @@ package com.ssafy.Homezakaya.model.service;
 import com.ssafy.Homezakaya.model.dto.UserInRoomDto;
 
 public interface UserInRoomService {
-<<<<<<< HEAD
-    boolean insertUserInRoom(UserInRoomDto userInRoom);
-    boolean deleteUserInRoom(String userId);
-    Integer getRoomId(String userId);
-=======
     boolean createUserInRoom(UserInRoomDto userInRoom);
     boolean removeUserInRoom(String userId);
-    int getRoomId(String userId);
->>>>>>> 8c125aa0904858b9495cd38f2a8e42b04ebe82c4
+    Integer getRoomId(String userId);
 }

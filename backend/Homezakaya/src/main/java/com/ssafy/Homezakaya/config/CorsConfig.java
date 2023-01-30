c23e292e0d290189829b22a9f9c56edc98467278
9jinseok <mer0120@naver.com>
2023-01-30 13:20:37 +0900

    gjs::refactor:: CorsConfig 추가

---
+++ backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.ssafy.Homezakaya.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173")
+                .allowCredentials(true)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                );
+    }
+}

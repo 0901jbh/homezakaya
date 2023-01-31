88d631a8be19dbb4b825d536b746bc0e3bf5b536
9jinseok <mer0120@naver.com>
2023-01-31 11:05:03 +0900

    gjs::refactor:: 비밀번호 체크 방식 변경

--- backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/model/dao/RoomDao.java
+++ backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/model/dao/RoomDao.java
@@ -10,7 +10,7 @@ public interface RoomDao {
     int insertRoom(RoomDto room);
     List<RoomDto> selectRooms();
     RoomDto selectRoom(int roomId);
-    RoomDto checkPassword(RoomDto room);
+    String selectPassword(int roomId);
     int enterRoom(int roomId);
     int quitRoom(int roomId);
     int deleteRoom(int roomId);

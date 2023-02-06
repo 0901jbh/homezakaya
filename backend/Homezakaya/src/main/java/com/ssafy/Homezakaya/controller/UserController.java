505729e71d2e55130408ac20747b0cf5b851bdc8
SujeongJo <suz.dev33@gmail.com>
2023-02-06 20:18:31 +0900

    jsj::feat:: 비밀번호 찾기 구현

--- backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/controller/UserController.java
+++ backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/controller/UserController.java
@@ -105,7 +105,7 @@ public class UserController {
         }
     }
 
-    //    // 메일인증
+    // 메일인증
     @PostMapping("/login/mailConfirm")
     public ResponseEntity<?> mailConfirm(@RequestParam String email) throws MessagingException, UnsupportedEncodingException {
         Map<String, Object> resultMap = new HashMap<>();
@@ -140,11 +140,30 @@ public class UserController {
         return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    // pw 찾기 (userId, email 인증)
-//    @GetMapping("/login/findPassword")
-//    public ResponseEntity<?> findPassword(@RequestParam String email) throws MessagingException, UnsupportedEncodingException {
-//
-//    }
+    // pw 찾기 (email로 임시 password 전송)
+    @GetMapping("/login/findPassword")
+    public ResponseEntity<?> findPassword(@RequestBody UserDto user) throws MessagingException, UnsupportedEncodingException {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        String email = user.getEmail();
+        String userId = user.getUserId();
+
+        if (userService.getUser(userId) != null && userService.getUser(email) !=null) {
+            String temPw = emailService.sendSimpleMessageForPassword(email);    // 임시 비밀번호 발송
+            log.info("임시 비밀번호 : " + temPw);
+            resultMap.put("temPw", temPw);
+
+            // 임시 비밀번호로 update
+            user.setPassword(temPw);
+            userService.modifyUser(user);
+
+            return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+
+        } else {
+            resultMap.put("message", "가입 정보가 존재하지 않습니다.");
+            return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 
     // 매너 도수 갱신
--- backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/model/service/EmailService.java
+++ backend/Homezakaya/src/main/java/com/ssafy/Homezakaya/model/service/EmailService.java
@@ -14,6 +14,7 @@ import javax.mail.internet.MimeMessage;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Random;
+import java.util.UUID;
 
 //@PropertySource("classpath:application.properties")
 @Slf4j
@@ -61,6 +62,33 @@ public class EmailService {
         return key.toString();
     }
 
+    // 메일 양식 작성
+    public MimeMessage createMessageForPassword(String to) throws MessagingException, UnsupportedEncodingException {
+//        log.info("보내는 대상 : " + to);
+//        log.info("인증 번호 : " + ePw);
+        MimeMessage message = javaMailSender.createMimeMessage();
+        
+        // 임시 비밀번호 생성 (UUID)
+        String temPw = UUID.randomUUID().toString().replace("-","").substring(0,10);
+        System.out.println(temPw);  // 확인
+
+        message.addRecipients(MimeMessage.RecipientType.TO, to);
+        message.setSubject("임시 비밀번호");
+
+        String msg = "";
+        msg += "<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">임시 비밀번호 입니다.</h1>";
+        msg += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">임시 비밀번호를 입력해 주세요.</p>";
+        msg += "<div style=\"padding-right: 30px; padding-left: 30px; margin: 32px 0 40px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">";
+        msg += temPw;
+        msg += "</td></tr></tbody></table></div>";
+
+        message.setText(msg, "utf-8", "html");
+        message.setFrom(new InternetAddress(id, "Admin"));
+
+        return message;
+    }
+
+
     // 메일 발송
 
     /**
@@ -71,6 +99,24 @@ public class EmailService {
      */
     public String sendSimpleMessage(String to) throws MessagingException, UnsupportedEncodingException {
         MimeMessage message = createMessage(to);    // 메일로 전송할 내용 담기
+        try {
+            javaMailSender.send(message);
+        } catch (MailException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+        return ePw; // 메일로 보냈던 인증 코드 서버로 리턴
+    }
+    // 메일 발송
+
+    /**
+     * @param to : 인증번호를 받을 메일 주소
+     * @return
+     * @throws MessagingException
+     * @throws UnsupportedEncodingException
+     */
+    public String sendSimpleMessageForPassword(String to) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = createMessageForPassword(to);    // 메일로 전송할 내용 담기
         try {
             javaMailSender.send(message);
         } catch (MailException e) {

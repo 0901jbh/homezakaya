748aa8b7167a3356b89b107c6198a92d98a8e84d
SujeongJo <suz.dev33@gmail.com>
2023-02-08 12:33:03 +0900

    jsj::feat::login axios

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,33 @@
 <template>
-  <div class='router-view'>
-    <RouterView/>
+  <div class="router-view">
+    <RouterView />
   </div>
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 import { useStore } from "vuex";
 
 const store = useStore();
 window.onunload = function (event) {
-  
   if (self.screenTop > 9000) {
     // 브라우저 닫힘
     //alert('닫힘');
     event.preventDefault();
-  store.dispatch("userModule/userLogout", sessionStorage.getItem("userId"));
-  }   
+    store.dispatch("userModule/userLogout", sessionStorage.getItem("userId"));
+  }
   // else {
   //     if(document.readyState == "complete") {
   //      // 새로고침
-  //      //alert('새로고침'); 
-  //     } 
+  //      //alert('새로고침');
+  //     }
   //     else if(document.readyState == "loading") {
   //       // 다른 사이트로 이동
   //       //alert('이동');
-        
+
   //     }
   // }
-}
+};
 </script>
 
 <style scoped>
--- frontend/src/views/lounge/signup/SignUpView.vue
+++ frontend/src/views/lounge/signup/SignUpView.vue
@@ -8,31 +8,51 @@
       <div class="signup-form">
         <el-form :model="form" label-width="150px">
           <el-form-item label="아이디">
-            <el-input v-model="form.id" placeholder="아이디를 입력해주세요" clearable>
+            <el-input
+              v-model="form.id"
+              placeholder="아이디를 입력해주세요"
+              clearable
+            >
               <template #append>
                 <el-button @click="idcheck" type="info">중복확인</el-button>
               </template>
             </el-input>
           </el-form-item>
           <el-form-item label="비밀번호">
-            <el-input v-model="form.password" placeholder="비밀번호를 입력해주세요" show-password />
+            <el-input
+              v-model="form.password"
+              placeholder="비밀번호를 입력해주세요"
+              show-password
+            />
           </el-form-item>
           <el-form-item label="재입력">
-            <el-input v-model="form.password2" placeholder="비밀번호를 재입력해주세요" show-password>
+            <el-input
+              v-model="form.password2"
+              placeholder="비밀번호를 재입력해주세요"
+              show-password
+            >
               <template #append>
                 <el-button @click="isCorrect">비밀번호확인</el-button>
               </template>
             </el-input>
           </el-form-item>
           <el-form-item label="닉네임">
-            <el-input v-model="form.nickname" placeholder="닉네임을 입력해주세요" clearable>
+            <el-input
+              v-model="form.nickname"
+              placeholder="닉네임을 입력해주세요"
+              clearable
+            >
               <template #append>
                 <el-button @click="nicknamecheck">중복확인</el-button>
               </template>
             </el-input>
           </el-form-item>
           <el-form-item label="이름">
-            <el-input v-model="form.name" placeholder="이름을 입력해주세요" clearable />
+            <el-input
+              v-model="form.name"
+              placeholder="이름을 입력해주세요"
+              clearable
+            />
           </el-form-item>
           <el-form-item label="성별">
             <!-- <el-input v-model="form.gender" placeholder="성별선택으로 바꿀예정" clearable /> -->
@@ -43,10 +63,19 @@
           </el-form-item>
           <el-form-item label="생년월일">
             <!-- <el-input v-model="form.birth" placeholder="생년월일을 입력해주세요 ex)1996-05-18" clearable /> -->
-            <el-date-picker v-model="form.birth" type="date" placeholder="생년월일을 입력해주세요" style="width: 100%" />
+            <el-date-picker
+              v-model="form.birth"
+              type="date"
+              placeholder="생년월일을 입력해주세요"
+              style="width: 100%"
+            />
           </el-form-item>
           <el-form-item label="이메일">
-            <el-input v-model="form.email" placeholder="이메일을 입력해주세요" clearable>
+            <el-input
+              v-model="form.email"
+              placeholder="이메일을 입력해주세요"
+              clearable
+            >
               <template #append>
                 <el-button @click="emailsend">인증</el-button>
               </template>
@@ -61,16 +90,26 @@
         </el-form-item> -->
           <el-form-item label="주량">
             <!-- <el-input v-model="form.alcohol" placeholder="주량을 입력해주세요 (소주 기준)" clearable /> -->
-            <el-input-number v-model="form.alcohol" size="large" :precision="1" :step="0.5" :min="0" />
+            <el-input-number
+              v-model="form.alcohol"
+              size="large"
+              :precision="1"
+              :step="0.5"
+              :min="0"
+            />
             <span class="el-form-item__label">&nbsp;잔 (소주)</span>
           </el-form-item>
           <el-form-item>
             <div class="btn">
-              <RouterLink to="/rooms" style="text-decoration: none"><el-button type="info" size="large"
-                  @click="onSubmit">가입 완료</el-button></RouterLink>
+              <RouterLink to="/rooms" style="text-decoration: none"
+                ><el-button type="info" size="large" @click="onSubmit"
+                  >가입 완료</el-button
+                ></RouterLink
+              >
             </div>
             <div class="btn">
-              <RouterLink to="/" style="text-decoration: none"><el-button type="info" size="large">돌아가기</el-button>
+              <RouterLink to="/" style="text-decoration: none"
+                ><el-button type="info" size="large">돌아가기</el-button>
               </RouterLink>
             </div>
           </el-form-item>
@@ -88,8 +127,26 @@ import { reactive, ref } from "vue";
 import { ElMessage, ElMessageBox } from "element-plus";
 import HeaderItem from "../menu/HeaderItem.vue";
 import FooterItem from "../menu/FooterItem.vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
-const form = reactive({
+const router = useRouter();
+const store = useStore();
+
+// const form = reactive({
+//   id: "",
+//   password: "",
+//   password2: "",
+//   nickname: "",
+//   name: "",
+//   gender: "",
+//   birth: "",
+//   email: "",
+//   emailcheck: "",
+//   alcohol: 0,
+// });
+
+const data = ref({
   id: "",
   password: "",
   password2: "",
@@ -104,11 +161,23 @@ const form = reactive({
 
 let flag = ref(false);
 
-const onSubmit = () => {
+const onSubmit = async () => {
+  await store.dispatch("userModule/createUser", {
+    userId: date.value.id,
+    password: form.password,
+    nickname: form.value.nickname,
+    username: form.value.name,
+    gender: form.value.gender,
+    birth: form.value.birth,
+    email: form.value.email,
+    alcoholPoint: form.value.alcohol,
+  });
+  router.push({ name: "rooms" });
   console.log("submit!");
 };
 
-const idcheck = () => {
+const idcheck = async () => {
+  await store.dispatch("userModule/createUser", form.value.id);
   console.log("id check!");
 };
 
@@ -122,7 +191,7 @@ const emailsend = () => {
     confirmButtonText: "OK",
     cancelButtonText: "Cancel",
   })
-    .then(({ }) => {
+    .then(({}) => {
       console.log("email confirm!");
       ElMessage({
         type: "success",
@@ -137,9 +206,9 @@ const emailsend = () => {
     });
 };
 
-// const emailconfirm = () => {
-//   console.log('email confirm!')
-// }
+const emailconfirm = () => {
+  console.log("email confirm!");
+};
 
 function isCorrect() {
   if (form.password == form.password2) {
@@ -158,7 +227,7 @@ function isCorrect() {
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-image: url('@/assets/images/main.png');
+  background-image: url("@/assets/images/main.png");
   background-size: cover;
 }
 
@@ -170,7 +239,7 @@ function isCorrect() {
   background: hsl(229.41, 18.68%, 17.84%, 80%);
   height: 80%;
   width: 1000px;
-  color: #CBCBCB;
+  color: #cbcbcb;
 }
 
 .signup-title {

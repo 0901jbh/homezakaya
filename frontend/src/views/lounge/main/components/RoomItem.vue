79038bad29efdda2a981ca0f303be0023fc84797
9jinseok <mer0120@naver.com>
2023-02-09 13:12:35 +0900

    gjs::fix:: 방 isPrivate 관련 수정

--- frontend/src/views/lounge/main/components/RoomItem.vue
+++ frontend/src/views/lounge/main/components/RoomItem.vue
@@ -14,7 +14,7 @@
 		<div class="content">
 			<div>{{ props.room.category }}</div>
 			<div>{{ props.room.personCount }} / {{ props.room.personLimit }}</div>
-			<div v-if="props.room.isPrivate">
+			<div v-if="props.room.private">
 				<img src="@/assets/images/party.png" alt="party img" style="height: 80%;">
 			</div>
 			<div v-else>
@@ -83,7 +83,7 @@ const data = ref({
 
 const clickRoomIcon = () => {
 	console.log(props.room);
-	if(props.room.isPrivate){
+	if(props.room.private){
 		privatePopOpen()
 	}
 	else{

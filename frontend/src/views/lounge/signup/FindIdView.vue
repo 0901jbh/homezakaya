76e1d07fc5ce08eed080bcb3ef7b92c198a93916
jbh <0901jbh@naver.com>
2023-02-12 21:24:32 +0900

    jbh::feat::아이디 비밀번호 찾기 뒤로가기

--- frontend/src/views/lounge/signup/FindIdView.vue
+++ frontend/src/views/lounge/signup/FindIdView.vue
@@ -27,6 +27,12 @@
       <div v-if="data.emailChecked" class="id-result">
         당신의 아이디는 {{ data.id }} 입니다.
       </div>
+        <el-form-item>
+            <el-button type="info" size="large" style="width: 300px;">
+              <RouterLink to="/homeview" style="color:black">뒤로 가기</RouterLink>
+            </el-button>
+          </el-form-item>
+
     </div>
   </div>
   <footer>
@@ -114,7 +120,9 @@ const emailsend = async () => {
   font-family: "dodum";
   src: url("@/assets/fonts/GowunDodum-Regular.ttf");
 }
-
+a {
+  text-decoration: none;
+}
 .container {
   display: flex;
   justify-content: center;

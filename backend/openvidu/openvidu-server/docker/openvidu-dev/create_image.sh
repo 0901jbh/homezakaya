52df57790094ecd37f15cd19732f3361cc19895e
jbh <0901jbh@naver.com>
2023-02-16 17:35:16 +0900

    jbh::feat refactoring

--- backend/openvidu/openvidu-server/docker/openvidu-dev/create_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VERSION=$1
-if [[ ! -z $VERSION ]]; then
-    cp ../../target/openvidu-server-*.jar ./openvidu-server.jar
-    docker build --pull --no-cache --rm=true -t openvidu/openvidu-dev:$VERSION .
-    rm ./openvidu-server.jar
-else
-    echo "Error: You need to specify a version as first argument"
-fi
